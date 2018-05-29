f69410cce87f38bb417d915428482c8f8c3d0548
Shyam Jos <shyamjosepp@gmail.com>
2018-05-29 21:39:15 +0530

    Add files via upload

---
+++ find-all-table-size-mysql.sql
@@ -0,0 +1,10 @@
+SELECT 
+    table_name AS `Table`, 
+    round(((data_length + index_length) / 1024 / 1024), 2) `Size in MB` 
+FROM information_schema.TABLES 
+WHERE table_schema = "$DB_NAME"
+    AND table_name = "$TABLE_NAME";
+
+
+
+
